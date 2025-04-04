61eb43875cd079c4785ec2ad40716e8c17da2b63
TimOleksii <smykovoleksii@gmail.com>
2025-04-04 22:45:04 +0200

    Wortschatz.py

--- Wortschatz.py
+++ Wortschatz.py
@@ -37,7 +37,7 @@ import random
 import requests
 
 # Настройки OpenRouter
-API_KEY = "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"
+API_KEY = st.secrets["API_KEY"] 
 API_URL = "https://openrouter.ai/api/v1/chat/completions"
 HEADERS = {
     "Authorization": f"Bearer {API_KEY}",
